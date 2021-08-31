48b6e4a8b5fa2246b3defde65cad763ec4a6168e
GennadiySerikov <serikov.gen@yandex.ru>
2021-08-31 11:31:04 +0300

    feat: lesson2.ex1 ready

--- lesson2/Program.cs
+++ lesson2/Program.cs
@@ -4,9 +4,19 @@ namespace lesson2
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args) {
+            GetAvarageDayTempreture();
+        }
+
+        static void GetAvarageDayTempreture() {
+            Console.Write("Введите t(min), C за сутки: ");
+            double minTemp = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите t(max), C за сутки: ");
+            double maxTemp = Convert.ToDouble(Console.ReadLine());
+
+            double result = (minTemp + maxTemp) / 2;
+            Console.WriteLine($"Средняя температура за сутки была: {result}");
         }
     }
 }
