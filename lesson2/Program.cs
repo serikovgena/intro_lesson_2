a024d10c8786d9553e54c47f70d8a1beadab9cdf
GennadiySerikov <serikov.gen@yandex.ru>
2021-09-02 00:01:27 +0300

    feat: lesson2. ex4 ready

--- lesson2/Program.cs
+++ lesson2/Program.cs
@@ -5,9 +5,10 @@ namespace lesson2
     class Program
     {
         static void Main(string[] args) {
-            //GetAvarageDayTempreture();
-            //GetMonthByNumber();
+            GetAvarageDayTempreture();
+            GetMonthByNumber();
             CheckParityOfNumber();
+            PrintCheck();
         }
 
         static void GetAvarageDayTempreture() {
@@ -31,6 +32,36 @@ namespace lesson2
             Console.Write("Введите целое число: ");
             int number = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Указанное число {number} { (number % 2 == 0 ? "четное" : "нечетное") }");
+        }
+
+        static void PrintCheck() {
+            int checkNumber = 5844;
+            string fuelTitle = "Топливо дизельное ЭКТО Diesel (ДТ-Л-К5)";
+            DateTime date = new DateTime(2015, 8, 7, 16, 33, 0);
+
+            decimal price = 34.50m;
+            decimal volume = 289.855m;
+            decimal sum = 10000.00m;
+
+            string checkView = 
+$@"************************************************
+        ООО ЛУКОЙЛ-Уралнефтепродукт      
+            Казанская сеть АЗС           
+          АЗС 16001 Космонавтов          
+Чек № {checkNumber}                     
+ТРК № 8 {fuelTitle}                     
+{price} X {volume} Л                    
+{date.ToString("dd.MM.yy HH:mm"), -10}            
+Тип оплаты: Наличные                               
+Тип скидки: Округление в пользу клиента            
+==================================================
+ИТОГ                                      {sum}
+ККМ 00023114                ИНН 007453011758 #5844
+
+                       ФП                         
+**************************************************";
+
+            Console.WriteLine(checkView);
         }
     }
 }
